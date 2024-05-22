40d7474be7a43d22dfe618142e3c38d3f53c885a
pedrofelipe29 <149617467+pedrofelipe29@users.noreply.github.com>
2024-05-22 15:34:46 -0300

    Add files via upload

--- index.ts
+++ index.ts
@@ -24,9 +24,9 @@ var entrada = require('prompt-sync')();
 
 var r = entrada('qual o modelo de resolução que você deseja?');
 var d = entrada('qual é a dificuldade que você deseja?');
-var t = entrada('true or false?');
+var tipoPersonagem = entrada('true or false?');
 
-console.log('essa é sua resolução' + r, 'esse é o nivel de dificuldade' + d, 'raytrancing:' + t)
+console.log('essa é sua resolução' + r, 'esse é o nivel de dificuldade' + d, 'raytrancing:' + tipoPersonagem)
 
 
 
@@ -93,6 +93,66 @@ if (infoMonstro) {
 
 
 //atv=04
+
+class Personagem {
+    nome: string;
+    hp: number;
+    mp: number;
+    forca: number;
+    defesa: number;
+    agilidade: number;
+    tipo: string;
+
+    
+    static TIPO_GUERREIRO = "Guerreiro";
+    static TIPO_MAGO = "Mago";
+    static TIPO_ASSASINO= "Assasino";
+    static TIPO_ARQUEIRO = "Arqueiro"
+
+    constructor(nome: string, tipo:string, hp: number = 100, mp: number = 70, forca: number = 60, defesa: number = 40, agilidade: number = 25) {
+        this.nome = nome;
+        this.hp = hp;
+        this.mp = mp;
+        this.forca = forca;
+        this.defesa = defesa;
+        this.agilidade = agilidade;
+        this.tipo = tipo;
+    }
+}
+
+    this.nome = nomePersonagem;
+
+
+
+
+console.log(Personagem.TIPO_GUERREIRO); 
+console.log(Personagem.TIPO_MAGO); 
+console.log(Personagem.TIPO_ASSASINO); 
+console.log(Personagem.TIPO_ARQUEIRO);
+
+var entrada = require('prompt-sync')();
+var nomePersonagem = entrada('informe o nome do personagem? ');
+var tipoPersonagem = entrada('qual o tipo de personagem vc deseja criar?\n1 - guerreiro\n2 - mago\n');
+
+let p = new Personagem(nomePersonagem, tipoPersonagem);
+
+if (tipoPersonagem === Personagem.TIPO_MAGO) {
+    p.mp += 30;
+} else if (tipoPersonagem === Personagem.TIPO_GUERREIRO) {
+    p.hp += 30;
+    p.forca += 15;
+} else if (tipoPersonagem === Personagem.TIPO_ASSASINO) {
+    p.agilidade += 15;
+} else if (tipoPersonagem=== Personagem.TIPO_ARQUEIRO) {
+    p.agilidade += 5;
+    p.forca += 7;
+    p.hp += 5;
+} else {
+    throw new Error("Tipo de personagem inválido!");
+}
+
+console.log(p);
+
 
 
 
